51ce13305835245c8964f2436a273e1f348a7023
nsedan <naim.alejandro.sedan@gmail.com>
2020-08-25 21:55:03 +0000

    added: edit and delete from db

--- mongo_project.py
+++ mongo_project.py
@@ -29,32 +29,6 @@ def show_menu():
     return option
 
 
-def get_record():
-    print("")
-    first = input("Enter first name > ")
-    last = input("Enter last name > ")
-
-    try:
-        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
-    except:
-        print("Error accessing the database")
-
-    if not doc:
-        print("")
-        print('Error! No results found.')
-
-    return doc
-
-
-def find_record():
-    doc = get_record()
-    if doc:
-        print("")
-        for k, v in doc.items():
-            if k != "_id":
-                print(k.capitalize() + ": " + v.capitalize())
-
-
 def add_record():
     print("")
     first = input("Enter first name > ")
@@ -73,8 +47,77 @@ def add_record():
         coll.insert_one(new_doc)
         print("")
         print("Document inserted")
-    except:
+    except Exception:
         print("Error accessing the database")
+
+
+def get_record():
+    print("")
+    first = input("Enter first name > ")
+    last = input("Enter last name > ")
+
+    try:
+        doc = coll.find_one({'first': first.lower(), 'last': last.lower()})
+    except Exception:
+        print("Error accessing the database")
+
+    if not doc:
+        print("")
+        print('Error! No results found.')
+
+    return doc
+
+
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {'$set': update_doc})
+            print("")
+            print("Document updated")
+        except Exception:
+            print("Error accessing the database")
+
+
+def delete_record():
+    doc = get_record()
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + " [" + v + "] > ")
+
+        print("")
+        confirmation = input(
+            "Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.delete_one(doc)
+                print("Document deleted!")
+            except Exception:
+                print("Error accessing the database")
+        else:
+            print("Document not deleted")
 
 
 def main_loop():
@@ -85,9 +128,9 @@ def main_loop():
         elif option == "2":
             find_record()
         elif option == "3":
-            print("You have selected option 3")
+            edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
